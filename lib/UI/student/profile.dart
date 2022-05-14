80d7461c32dc38d024c21d0d6df217a64dc02489
Anu Kumari <lit2019072@iiitl.ac.in>
2022-05-14 22:10:02 +0530

    ops

--- lib/UI/student/profile.dart
+++ lib/UI/student/profile.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:college_gate/UI/student/idcardview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
